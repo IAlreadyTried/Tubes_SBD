e2d1bb27f797a4309321d5f26dda3dec95edeea0
ganteng <erastusganteng1@gmail.com>
2022-04-26 22:55:48 +0700

    nambahin dulu

--- Moovit_Copy/resources/views/home.blade.php
+++ Moovit_Copy/resources/views/home.blade.php
@@ -4,10 +4,26 @@
     <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 " style="background-color:rgb(247, 153, 153);height:5000px;padding:0px;">
         <div id="gambar-kiri-atas" style="" class="bungkus-kiri-atas">
             <div style="" class="row baris-row">
-                <h2>keren</h2>
+                <div style="text-align: center;color:antiquewhite;width:175px;" class="col-5 align-self-end">
+                    <div style="position: relative">
+                        <img style="float: left;width:20px;position:absolute;left:17px;top:5px;" src="img/moovit_logo2.png" alt="">
+                        <h2 style="font-size: 23px">moovit</h2>
+                    </div>
+                </div>
+                <div class="col-3 align-self-end">
+                    <h2 style="font-size: 15px;color:rgb(174, 173, 173);">Waduoo</h2>
+                </div>
             </div>
             <div class="row baris-row">
-                <h2>Mantap</h2>
+                <div class="col-3">
+                    <h5>Ashiap</h5>
+                </div>
+                <div class="col-3">
+                    <h5>Ashiap</h5>
+                </div>
+                <div class="col-3">
+                    <h5>Ashiap</h5>
+                </div>
             </div>
         </div>
         <h1>Hello, World!</h1>
