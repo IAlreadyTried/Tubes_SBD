5613bc63a275191259ed1fe4999a00791ac4269a
ganteng <erastusganteng1@gmail.com>
2022-04-29 23:34:29 +0700

    Nambahin lagi gaes

--- Moovit_Copy/resources/views/home.blade.php
+++ Moovit_Copy/resources/views/home.blade.php
@@ -1,32 +1,8 @@
 @extends('layout.main')
 @section('container-1')
-<div class="row">
-    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 " style="background-color:rgb(247, 153, 153);height:5000px;padding:0px;">
-        <div id="gambar-kiri-atas" style="" class="bungkus-kiri-atas">
-            <div style="" class="row baris-row">
-                <div style="text-align: center;color:antiquewhite;width:175px;" class="col-5 align-self-end">
-                    <div style="position: relative">
-                        <img style="float: left;width:20px;position:absolute;left:17px;top:5px;" src="img/moovit_logo2.png" alt="">
-                        <h2 style="font-size: 23px">moovit</h2>
-                    </div>
-                </div>
-                <div class="col-3 align-self-end">
-                    <h2 style="font-size: 15px;color:rgb(174, 173, 173);">Waduoo</h2>
-                </div>
-            </div>
-            <div class="row baris-row">
-                <div class="col-3">
-                    <h5>Ashiap</h5>
-                </div>
-                <div class="col-3">
-                    <h5>Ashiap</h5>
-                </div>
-                <div class="col-3">
-                    <h5>Ashiap</h5>
-                </div>
-            </div>
+    <div style="margin: 0px; height:125px;color:rgb(41, 41, 41);" class="container row bg-dark">
+        <div class="col-2">
+            <h1>Hi</h1>
         </div>
-        <h1>Hello, World!</h1>
     </div>
-</div>
 @endsection
--- Moovit_Copy/resources/views/layout/main.blade.php
+++ Moovit_Copy/resources/views/layout/main.blade.php
@@ -47,7 +47,36 @@
         </div>
       </nav> --}}
     <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 " style="background-color:rgb(247, 153, 153);height:5000px;padding:0px;min-width:307px;">
+            <div id="gambar-kiri-atas" style="" class="bungkus-kiri-atas">
+                <div style="" class="row baris-row">
+                    <div style="text-align: center;color:rgb(237, 237, 237);width:175px;padding:0px;" class="col-5 align-self-end">
+                        <div style="position: relative">
+                            <img style="float: left;width:20px;position:absolute;left:30px;top:5px;" src="img/moovit_logo2.png" alt="">
+                            <h2 style="font-size: 23px">moovit</h2>
+                        </div>
+                    </div>
+                    <div class="col-3 align-self-end" style="text-align:left;padding:0px;">
+                        <h2 style="font-size: 15px;color:rgb(234, 234, 234);">Waduoo</h2>
+                        {{-- <img src="" alt=""> --}}
+                    </div>
+                </div>
+                <div class="row baris-row justify-content-start">
+                    <div style="width:80px;margin-left:20px;" class="col-3 align-self-end link-sidebar-kiri">
+                        <a href="/"><h5>DIRECTIONS</h5></a>
+                    </div>
+                    <div style="padding-right:5px" class="col-3 align-self-end link-sidebar-kiri">
+                        <a href="/lines"><h5>LINES</h5></a>
+                    </div>
+                    <div style="width: 60px" class="col-3 align-self-end link-sidebar-kiri">
+                        <a href="/alerts"><h5>ALERTS</h5></a>
+                    </div>
+                </div>
+            </div>
         @yield('container-1')
+        </div>
+    </div>
         {{-- <div class="row">
             <div class="col-3" style="background-color:rgb(247, 153, 153);height:5000px;">
             </div>
