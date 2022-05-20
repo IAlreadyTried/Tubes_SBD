1a6da513a0b6a26a077382a037d747c779c1faf9
Murni11 <murniwatisiahaan11@gmail.com>
2022-05-20 17:08:00 +0700

    rapikan login/register

--- Moovit_Copy/resources/views/login.blade.php
+++ Moovit_Copy/resources/views/login.blade.php
@@ -1,3 +1,5 @@
+<link rel="icon" type="image/x-icon" href="/img/moovit_logo2.png">
+<title>Moovit | Login</title>
 <div class="section">
      <div class="container"> 
          <div class="form"> 
@@ -6,13 +8,15 @@
              <script src="login.js"></script>
                  <div> 
                      <span class="brand"><img src="img/moovit_logo2.png"><small>Moovit</small></span> 
+                     <h2 class="mytitle" >L O G I N</h2>
                      <div class="form-inputs"> <input type="text" placeholder="Username"> 
                      <div class="password"> <input id="password" type="password" placeholder="Password"> <span class="showpass" onclick="toggle()"> <img id="changepasseye" src="https://i.imgur.com/d1M6y1W.jpg"> </span> <p class="random_password"></p> </div> 
-                     <input id="submit_button" type="submit"> <p class="login-text">Don't have an account? <a href="/register">Sign Up</a></p> 
+                     <input id="submit_button" type="submit"> <p class="login-text">Don't have an account? <a href="/register">Register</a></p> 
                     </div>
                 </div>
              </div> 
-                <div class="right-side"><img src="img/moovit_logo2.png" width="1500px" > 
+            <div class="right-side">
+                <img src="img/moovit_logo2.png" width="1500px" > 
             </div> 
         </div> 
     </div>
--- Moovit_Copy/resources/views/register.blade.php
+++ Moovit_Copy/resources/views/register.blade.php
@@ -1,3 +1,5 @@
+<link rel="icon" type="image/x-icon" href="/img/moovit_logo2.png">
+<title>Moovit | Register</title>
 <div class="section">
      <div class="container"> 
          <div class="form"> 
@@ -6,12 +8,13 @@
              <script src="login.js"></script>
                  <div> 
                      <span class="brand"><img src="img/moovit_logo2.png"><small>Moovit</small></span> 
+                     <h2 class="mytitle" style="margin-bottom: -20px"  >R E G I S T E R</h2>
                      <div class="form-inputs"> 
                      <input type="text" placeholder="Username">
                      <input type="text" placeholder="Name">
                      <input type="text" placeholder="Email Address"> 
                      <div class="password"> <input id="password" type="password" placeholder="Password"> <span class="showpass" onclick="toggle()"> <img id="changepasseye" src="https://i.imgur.com/d1M6y1W.jpg"> </span> <p class="random_password"></p> </div> 
-                     <input id="submit_button" type="submit"> <p class="login-text">Already have an account? <a href="/login">Sign in</a></p> 
+                     <input id="submit_button" type="submit"> <p class="login-text">Already have an account? <a href="/login">Login</a></p> 
                     </div>
                 </div>
              </div> 
