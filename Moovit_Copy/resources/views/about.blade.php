2f22b1bb4a3174531c4f2729418e2ba7dbe7e14b
NicholasSalim <89775307+NicholasSalim@users.noreply.github.com>
2022-05-20 20:10:14 +0700

    halaman about

---
+++ Moovit_Copy/resources/views/about.blade.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Moovit | About</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="vendor/aos/aos.css" rel="stylesheet">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Moderna - v4.8.0
+  * Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1 class="text-light"><a href="/"><span><img src="img/moovit_logo2.png" class="img-fluid rounded float-right" width="30" height="120" alt="">Moovit</span></a></h1>
+        
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active " href="index.html">Mass Solutions</a></li>
+          <li><a href="about.html">On-Demand</a></li>
+          <li><a href="services.html">Industries</a></li>
+          <li><a href="portfolio.html">Mobility App</a></li>
+          <li><a href="team.html">Resources</a></li>
+          <li><a href="blog.html">Company</a></li>
+          
+          <li><a href="contact.html">Contact Us</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex justify-cntent-center align-items-center" >
+    <div id="heroCarousel" class="container carousel carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+
+      <!-- Slide 1 -->
+      <div class="carousel-item active">
+        <div class="carousel-container">
+          <h2 class="animate__animated animate__fadeInDown">Autonomous Ride-Sharing Services by Mobileye and Moovit</h2>
+          <p class="animate__animated animate__fadeInUp">Helping people get around their city with safe, affordable and eco friendly transportation enabled by self driving vehicles.</p>
+          <a href="" class="btn-get-started animate__animated animate__fadeInUp">Learn More</a>
+        </div>
+      </div>
+
+      <!-- Slide 2 -->
+      <div class="carousel-item">
+        <div class="carousel-container">
+          <h2 class="animate__animated animate__fadeInDown">Shaping the future of Urban MoBility with MaaS</h2>
+          <p class="animate__animated animate__fadeInUp">Make getting from A to B a seamless and simple experience for your citizens with Moovit's Mobility-as-a-service (MaaS) solutions: Branded apps, mobile fare payments, on-demand transit, Big Data analytics, and more.</p>
+          <a href="" class="btn-get-started animate__animated animate__fadeInUp">Learn More</a>
+          <a href="" class="btn-get-started animate__animated animate__fadeInUp">Download E-Book</a>
+        </div>
+      </div>
+
+      <!-- Slide 3 -->
+      <div class="carousel-item">
+        <div class="carousel-container">
+          <h2 class="animate__animated animate__fadeInDown">Keeping Riders Connected with On-Demand</h2>
+          <p class="animate__animated animate__fadeInUp">Intelligent shared mobility planning and operations to fill your transit gaps, improve customer experience, and connect yoyr riders to a bigger mobility ecosystem.</p>
+          <a href="" class="btn-get-started animate__animated animate__fadeInUp">Learn More</a>
+          <a href="" class="btn-get-started animate__animated animate__fadeInUp">Download E-Book</a>
+        </div>
+      </div>
+
+      <!-- Slide 4 -->
+      <div class="carousel-item">
+        <div class="carousel-container">
+          <h2 class="animate__animated animate__fadeInDown">One App for All Your Mobility Needs</h2>
+          <p class="animate__animated animate__fadeInUp">Powered by the simplest and most intuitive mobility app in the world for the Plan-Pay-Ride experience. Branded for yout transit agency or city.</p>
+          <a href="" class="btn-get-started animate__animated animate__fadeInUp">Learn More</a>
+          <a href="" class="btn-get-started animate__animated animate__fadeInUp">See Feature</a>
+        </div>
+      </div>
+
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bx bx-chevron-left" aria-hidden="true"></span>
+      </a>
+
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bx bx-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Services Section ======= -->
+    <h2 style="text-align:center;">Our Core Solutions</h2>
+    <section class="services">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up">
+            <div class="icon-box icon-box-pink">
+              <div class="icon"><i class="bx bx-world"></i></div>
+              <h4 class="title"><a href="">White Label MaaS Application</a></h4>
+              <p class="description">Tap into the world-leding urban mobility app and offer your riders the city at their fingertips</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box icon-box-cyan">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4 class="title"><a href="">On-Demand (Microtransit)</a></h4>
+              <p class="description">Flexible, automated demand response solutions that keep your riders connected and fill in mobility gaps</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon-box icon-box-green">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4 class="title"><a href="">Urban Mobility Analytics</a></h4>
+              <p class="description">Plan and understand mobility trends with greater accuracy to solve your biggest challenges</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon-box icon-box-blue">
+              <div class="icon"><i class="bx bx-dollar-circle"></i></div>
+              <h4 class="title"><a href="">Mobile Payments Solutions</a></h4>
+              <p class="description">Touchless fare payments solutions for the ultimate 'plan,ride,pay' mobility experience for your customers</p>
+            </div>
+          </div>
+
+          
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+  
+     <!-- ======= Features Section ======= -->
+     <section class="features">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Moovit Awards</h2>
+      
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-md-5">
+            <img src="img/award-asia-smart-app-1.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-md-7 pt-4">
+            <h3>Leading Lifestyle, Social, and Entertainment App</h3>
+            
+            <ul>
+              <li><i class="bi bi-check"></i> Asia Smart App Awards</li>
+             
+            </ul>
+          </div>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-md-5 order-1 order-md-2">
+            <img src="img/Smart50_awardsrecipient_logo.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-md-7 pt-5 order-2 order-md-1">
+            <h3>Smart 50 Award</h3>
+           
+            <ul>
+              <li><i class="bi bi-check"></i>Smart Cities Connect</li>
+
+            </ul>
+          </div>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-md-5">
+            <img src="img/award-transport-ticketing.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-md-7 pt-5">
+            <h3>Real Time Passenger Information Implementation of the Year</h3>
+            
+            <ul>
+              <li><i class="bi bi-check"></i>Transport Ticketing Global</li>
+
+            </ul>
+          </div>
+        </div>
+
+       
+
+      </div>
+    </section><!-- End Features Section -->
+ 
+ 
+
+    
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
+
+    
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Company</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About Us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">MaaS Solutionss</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Supported Countrues</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Our Moovivr Community</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Moovit Carpool</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Careers</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Code of Conduct</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Tools</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Moovit Insights</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Developer Toolst</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Widgets for Websitest</a></li>
+            
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Contact Us</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Sales Inquiry</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">General Inquiry</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Moovit App Support</a></li>
+            
+            </ul>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <h3>Moovit</h3>
+            <p>Moovit, an Intel company, is a leading Mobility as a Service (MaaS) solutions provider and creator of the #1 urban mobility app</p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/ -->
+        
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="vendor/purecounter/purecounter.js"></script>
+  <script src="vendor/aos/aos.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
