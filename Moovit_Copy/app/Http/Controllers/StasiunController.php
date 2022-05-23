2ea3b0f8866196daa450bdf403033e619991ad6e
ganteng <erastusganteng1@gmail.com>
2022-05-24 00:15:44 +0700

    Ngepush asek

---
+++ Moovit_Copy/app/Http/Controllers/StasiunController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Stasiun;
+
+class StasiunController extends Controller
+{
+    public function Data() {
+        return view('home', [
+            'datanya' => Stasiun::all()
+        ]);
+    }
+}
