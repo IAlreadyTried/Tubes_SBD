302e40580da4d6b6707497f6271b1254793f836d
ganteng <erastusganteng1@gmail.com>
2022-05-26 18:18:44 +0700

    Dibalekkan

--- Moovit_Copy/public/ajax/pilihannya.php
+++ Moovit_Copy/public/ajax/pilihannya.php
@@ -10,5 +10,13 @@ $rows = [];
 ?>
 
 <?php foreach($rows as $row): ?>
-<div style="width: 100%;height:55px;background-color:salmon;border-bottom:5px black;box-sizing:border-box;"><?php echo $row['nama'];?></div>
+<div class="row" style="margin:0px;width: 100%;height:65px;background-color:rgb(255, 255, 255);border-bottom:1px solid rgb(60, 60, 60);box-sizing:border-box;z-index:1000;">
+    <div style="text-align:center;line-height:65px;" class="col-2"><img style="width: 20px" src="../img/location.png" alt=""></div>
+    <div style="line-height:17px;" class="col-10 d-flex align-items-center">
+        <div class="">
+            <div class="row"><?php echo $row['nama'];?></div>
+            <div class="row" style="font-size: 12px;color:#868686;"><?php echo $row['peran'];?> . <?php echo $row['namajalan'];?> . <?php echo $row['namakota'];?></div>
+        </div>
+    </div>
+</div>
 <?php endforeach; ?>
